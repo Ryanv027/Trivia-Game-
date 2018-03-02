59ebe41d220fa5d790a295d604d51a6adc92e630
Ryan Palermo <RyanPalermo@Ryans-MacBook-Pro.local>
2018-03-02 13:24:50 -0800

    updated timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,104 @@
-const quizContainer = document.getElementById('quiz');
+function startUp() {
+    //Calling our function that forms our questions
+    buildQuiz()
+        //creating the done button at the bottom of the quiz
+    $('.button-col').html('<button class="btn-primary button" id="submit">Done!</button>')
+        //setting up a timer at the beginning of the quiz
+    var counter = 120;
+    var intervalId = setInterval(timeIt, 1000)
+    var display = '2:00'
+    setInterval(convertTime, 1000)
 
 
+    function convertTime() {
+        var minutes = Math.floor(counter / 60)
+        var seconds = counter - (minutes * 60)
+
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        if (minutes === 0) {
+            minutes = "00";
+        } else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+
+        display = minutes + ":" + seconds;
+    }
+
+
+    function timeIt() {
+        console.log('hello')
+        counter--
+        $('#timer').html('Time left: ' + (display))
+        if (counter === 0) {
+            showResults()
+            clearInterval(intervalId)
+            $('#timer').html('Your Results!')
+            $("#restart").html(`<button class="btn-primary button" id="button restarting">Restart</button>`)
+            $('#restarting').on('click', function() {
+                console.log('wtf')
+                startUp()
+            })
+        }
+    }
+    //Creating an on click event that displays the results of the quiz when finished
+    $("#submit").on('click', function() {
+        showResults()
+        $("#restart").html(`<button class="btn-primary button" id="restarting">Restart</button>`)
+        $('#restarting').on('click', function() {
+            startUp()
+        })
+        clearInterval(intervalId)
+        $('#timer').html('Your Results!')
+    });
+}
+//Creating a function that will create our answers from our object array
+//creating a function that presents the results from the quiz
+function showResults() {
+
+    const answerContainers = $('#quiz').find('.answers')
+    let output = []
+    let numbCorrect = 0;
+    let incorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+        if (userAnswer === currentQuestion.correctAnswer) {
+            numbCorrect++
+        } else {
+            incorrect++
+        }
+    })
+
+    output.push(
+        `<p class="results">You got ${numbCorrect} answers correct!</p>
+        <p class="results">You got ${incorrect} answers incorrect...</p>`)
+
+    $('#quiz').html(output)
+}
+
+$('#start').on('click', function() {
+    startUp()
+})
 
 function buildQuiz() {
     const output = []
 
     myQuestions.forEach((currentQuestion, questionNumber) => {
         const answers = []
-        for (letter in currentQuestion.answers) {
+        for (let letter in currentQuestion.answers) {
             answers.push(
                 `<label>
                 <input type="radio" name="question${questionNumber}" value="${letter}">${letter}: ${currentQuestion.answers[letter]}
                 </label>`
             );
         }
-
         output.push(
             `<div class="question">
             ${currentQuestion.question} </div> 
@@ -22,9 +106,8 @@ function buildQuiz() {
             ${answers.join('')} </div>`
         );
     });
-    quizContainer.innerHTML = output.join('')
+    $('#quiz').html(output)
 }
-
 const myQuestions = [{
         question: "1. Who won the 2010 World Series?",
         answers: {
@@ -106,47 +189,3 @@ const myQuestions = [{
         correctAnswer: "a"
     },
 ];
-
-buildQuiz()
-
-function showResults() {
-    console.log('hello')
-    const answerContainers = quizContainer.querySelectorAll(".answers");
-
-    let output = []
-
-    let numbCorrect = 0;
-    console.log(numbCorrect)
-
-    let incorrect = 0;
-
-    console.log(incorrect)
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-
-
-
-        const answerContainer = answerContainers[questionNumber];
-
-        const selector = `input[name=question${questionNumber}]:checked`;
-
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-        console.log(userAnswer)
-
-        if (userAnswer === currentQuestion.correctAnswer) {
-            numbCorrect++
-
-        } else {
-            incorrect++
-        }
-    })
-
-    output.push(
-        `<p class="results">You got ${numbCorrect} answers correct!</p>
-        <p class="results">You got ${incorrect} answers incorrect... `)
-
-    quizContainer.innerHTML = output.join('')
-}
-
-$("#submit").on('click', function() {
-    showResults()
-});
